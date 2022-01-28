f5c813ddeeca025f3c3fedaa07174dc6519050ee
iskanderAB <iskanderabasi@gmail.com>
2022-01-28 16:40:08 +0200

    create splash screen with bootsplash library

---
+++ src/containers/initContainer/InitContainer.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const InitContainer = () => {
+  return (
+    <View style={{}}>
+      
+    </View>
+  );
+};
+
+export default InitContainer;
