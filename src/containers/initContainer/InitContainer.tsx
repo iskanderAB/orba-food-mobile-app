666e1219621fc56ca9abb25c93c07488613e15db
iskanderAB <iskanderabasi@gmail.com>
2022-01-31 18:21:39 +0200

    ass signUp screen and fix container folder position

--- src/containers/initContainer/InitContainer.tsx
+++ src/containers/initContainer/InitContainer.tsx
@@ -1,98 +1,30 @@
 import React from 'react';
 import { 
   View,
-  StyleSheet,
-  ImageBackground,
-  Image, 
-  ScrollView,
-  Dimensions} from 'react-native';
+  StyleSheet} from 'react-native';
 
 import PhoneForm from '../../components/phoneForm/PhoneForm';
-import OrbaText from '../../components/UI/orbaText/OrbaText';
-
-
-import Colors from '../../utils/colors/themColor';
-
-const {width, height} = Dimensions.get('window');
 
 const InitContainer = ({children} :{ children : JSX.Element }): JSX.Element => {
   return (
-    <ScrollView style={{backgroundColor: Colors.green}}>
-      <View style={styles.container}>
-        <View style={styles.slider}>
-          <ImageBackground 
-            source={require('../../res/images/foods.png')} 
-            resizeMode="cover"
-            style={styles.imageBackround}
-          >
-            <View style={styles.greenFilterContainer}/>
-          </ImageBackground>
-        </View>
-        <Image source={require('../../res/logos/orbaLogo.png')} style={styles.logo}/>
-        <View style={styles.subContainer}>
-          <PhoneForm/>
-        </View>
-        <OrbaText style={styles.version}>
-            الاصدار :1.0.0 
-        </OrbaText>
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <PhoneForm/>
+    </View>
   );
 };
 
 export default InitContainer;
+
 const logoSize = {
   width: 145,
   height: 150 
 }
 const styles = StyleSheet.create({
-  container: { 
-    height: height-30,
-    fontFamily: "Almarai"
-  },
-  slider: { 
-    width : '100%',
-    height: '40%',
-    backgroundColor: 'red'
-  },
-  imageBackround: {
-    flex: 1
-  },
-  greenFilterContainer : { 
-    flex: 1 ,
-    backgroundColor: Colors.green,
-    opacity: 0.7,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'flex-end'
-  },
-  logo: {
-    height: logoSize.height,
-    width: logoSize.width,
-    transform: [{translateY: logoSize.height*1.25}],
-    resizeMode: 'contain',
-    position: 'absolute',
-    alignSelf: 'center',
-    zIndex: 2,
-  },
-  subContainer:{ 
+  container:{ 
     marginTop: logoSize.height/2,
     flex:1,
     flexDirection: 'row',
     alignItems: 'flex-end',
-    paddingBottom: 10
+    paddingBottom: 30
   },
-  version: { 
-    color: Colors.gray,
-    textAlign: 'center',
-    fontSize: 14,
-  },
-  input: { 
-    borderColor: "gray",
-    width: "90%",
-    borderWidth: 1,
-    borderRadius: 10,
-    padding: 10,
-    alignSelf: 'center'
-  }
 })
---
+++ src/screens/signUp/SignUpWithPhone.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { 
+  View,
+  StyleSheet,
+  ImageBackground,
+  Image, 
+  ScrollView,
+  Dimensions} from 'react-native';
+
+import PhoneForm from '../../components/phoneForm/PhoneForm';
+import OrbaText from '../../components/UI/orbaText/OrbaText';
+import InitContainer from '../../containers/initContainer/InitContainer';
+
+
+import Colors from '../../utils/colors/themColor';
+
+const {width, height} = Dimensions.get('window');
+
+const SignUpWithPhone = ({children} :{ children : JSX.Element }): JSX.Element => {
+  return (
+    <ScrollView style={{backgroundColor: Colors.green}}>
+      <View style={styles.container}>
+        <View style={styles.slider}>
+          <ImageBackground 
+            source={require('../../res/images/foods.png')} 
+            resizeMode="cover"
+            style={styles.imageBackround}
+          >
+            <View style={styles.greenFilterContainer}/>
+          </ImageBackground>
+        </View>
+        <Image source={require('../../res/logos/orbaLogo.png')} style={styles.logo}/>
+          <InitContainer/>
+        <OrbaText style={styles.version}>
+            الاصدار :1.0.0 
+        </OrbaText>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default SignUpWithPhone;
+const logoSize = {
+  width: 145,
+  height: 150 
+}
+const styles = StyleSheet.create({
+  container: { 
+    height: height-30,
+    fontFamily: "Almarai"
+  },
+  slider: { 
+    width : '100%',
+    height: '40%',
+    backgroundColor: 'red'
+  },
+  imageBackround: {
+    flex: 1
+  },
+  greenFilterContainer : { 
+    flex: 1 ,
+    backgroundColor: Colors.green,
+    opacity: 0.7,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  logo: {
+    height: logoSize.height,
+    width: logoSize.width,
+    transform: [{translateY: logoSize.height*1.25}],
+    resizeMode: 'contain',
+    position: 'absolute',
+    alignSelf: 'center',
+    zIndex: 2,
+  },
+  version: { 
+    color: Colors.gray,
+    textAlign: 'center',
+    fontSize: 14,
+  },
+  input: { 
+    borderColor: "gray",
+    width: "90%",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    alignSelf: 'center'
+  }
+})
